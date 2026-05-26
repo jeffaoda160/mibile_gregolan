6dcfb93177606403fbfb310dffc263ae9e15a34c
jeffaoda160 <gabriel.gregolan.lima@escola.pr.gov.br>
2026-05-26 18:02:56 -0300

    atividadade 3

---
+++ ATT3.JS
@@ -0,0 +1,15 @@
+
+let soma = 0;
+for (let i = 1; i <= 5; i++) {
+    let nota = Number(prompt("Digite a nota " + i + ":"));
+    soma += nota;
+}
+let media = soma / 5;
+console.log("Média:", media);
+if (media >= 7) {
+    console.log("Aprovado");
+} else if (media >= 5) {
+    console.log("Recuperação");
+} else {
+    console.log("Reprovado");
+}
