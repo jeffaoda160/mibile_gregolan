36635092522b11cf84929c70901844ec6aaf2095
jeffaoda160 <gabriel.gregolan.lima@escola.pr.gov.br>
2026-06-30 17:29:33 -0300

    a

---
+++ aula1/ex04.js
@@ -0,0 +1,9 @@
+
+function tab(numero) {
+    console.log(`Tabuada do ${numero}`);
+    for (let i = 1; i <= 10; i++) {
+        console.log(`${numero} x ${i} = ${numero * i}`);
+    }
+}
+
+tab(5);
